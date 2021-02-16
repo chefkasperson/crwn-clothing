48f98eabcdd6b38fe32352db87d3a900cf203367
chefkasperson <chefkasperson@gmail.com>
2021-02-16 02:02:13 -0800

    added homepage component;

---
+++ src/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+            </div>
+        </div>
+    </div>
+)
