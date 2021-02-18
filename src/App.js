bee37626f5f4498d6700347e9e138f3c53230a83
chefkasperson <chefkasperson@gmail.com>
2021-02-18 03:25:46 -0800

    connected shop route to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './App.css';
-import HomePage from './pages/homepage/homepage.component.jsx'
 import { Route, Switch } from 'react-router-dom'
+import HomePage from './pages/homepage/homepage.component.jsx'
+import ShopPage from './pages/shop/shop.component.jsx'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div >
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
