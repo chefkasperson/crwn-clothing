f8a9e9246526710379e2fd053d973c24541ce39c
chefkasperson <chefkasperson@gmail.com>
2021-03-09 18:42:52 -0800

    added cart dropdown and started added toggle ability to cart-icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 import { auth } from '../../firebase/firebase.utils'
 
@@ -31,6 +32,7 @@ const Header = ({ currentUser }) => (
             }
             <CartIcon />
         </div>
+        <CartDropdown />
     </div>
 )
 
